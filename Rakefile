bad087e742194c4814fc4697c600cf517e991b8e
Jan Lindblom <janlindblom@fastmail.fm>
2018-03-27 08:21:30 +0300

    Fixed failing tests.

--- Rakefile
+++ Rakefile
@@ -1,9 +1,25 @@
 require "bundler/gem_tasks"
-require "rspec/core/rake_task"
 
 Dir.glob('rake/*.rake').each { |r| load r}
 
-RSpec::Core::RakeTask.new(:spec)
+begin
+  require 'rspec/core/rake_task'
+  namespace :spec do
+    RSpec::Core::RakeTask.new(:offline) do |t, task_args|
+      t.rspec_opts = "--tag offline"
+    end
 
+    RSpec::Core::RakeTask.new(:online) do |t, task_args|
+      t.rspec_opts = "--tag online"
+    end
 
-task :default => :spec
+    RSpec::Core::RakeTask.new(:all) do |t, task_args|
+      t.rspec_opts = "--tag online --tag offline"
+    end
+  end
+
+  task spec: "spec:all"
+
+  task default: :spec
+rescue LoadError
+end
--- spec/time_traveler_spec.rb
+++ spec/time_traveler_spec.rb
@@ -1,27 +1,27 @@
 RSpec.describe TimeTraveler do
-  it "has a version number" do
+  it "has a version number", offline: true do
     expect(TimeTraveler::VERSION).not_to be nil
   end
 
-  it "can find the timezone for Åland, Finland" do
+  it "can find the timezone for Åland, Finland", offline: true do
     tz_geta = TimeTraveler.find_timezone(19.847005, 60.374794)
     expect(tz_geta).to be_a TZInfo::Timezone
     expect(tz_geta).to eq TZInfo::Timezone.get("Europe/Mariehamn")
   end
 
-  it "can find the timezone for New Delhi, India" do
+  it "can find the timezone for New Delhi, India", offline: true do
     tz_nd = TimeTraveler.find_timezone(77.2090210, 28.6139390)
     expect(tz_nd).to be_a TZInfo::Timezone
     expect(tz_nd).to eq TZInfo::Timezone.get("Asia/Kolkata")
   end
 
-  it "can find the timezone for Palo Alto, USA" do
+  it "can find the timezone for Palo Alto, USA", offline: true do
     tz_pa = TimeTraveler.find_timezone(-122.1430190, 37.4418830)
     expect(tz_pa).to be_a TZInfo::Timezone
     expect(tz_pa).to eq TZInfo::Timezone.get("America/Los_Angeles")
   end
 
-  it "can find the timezone for Cape Town, South Africa" do
+  it "can find the timezone for Cape Town, South Africa", offline: true do
     tz_ct = TimeTraveler.find_timezone(18.4240550, -33.9248680)
     expect(tz_ct).to be_a TZInfo::Timezone
     expect(tz_ct).to eq TZInfo::Timezone.get("Africa/Johannesburg")
